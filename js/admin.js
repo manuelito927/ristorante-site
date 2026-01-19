03a033ab0ca11c328ef7e42d9b9e2f0a471f7008
manuelito927 <filosofiaefficace@gmail.com>
2026-01-19 10:51:50 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -265,7 +265,7 @@ function readPayload(card) {
   // CREA nuovo prodotto
   if (createBtn) {
     createBtn.onclick = async () => {
-      const allergens = Array.from(document.querySelectorAll("#tab-menu .alg:checked").map(el => el.value);
+const allergens = [];
       const payload = {
         name: $("name").value.trim(),
         name_en: ($("name_en") ? $("name_en").value.trim() : ""),
