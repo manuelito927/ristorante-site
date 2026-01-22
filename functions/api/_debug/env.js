746bca18d86b775b8ce6ef3548fd9b4772102b91
manuelito927 <filosofiaefficace@gmail.com>
2026-01-22 10:33:40 +0100

    Update env.js

--- functions/api/_debug/env.js
+++ functions/api/_debug/env.js
@@ -1,9 +1,11 @@
 export async function onRequestGet({ env }) {
   return new Response(
     JSON.stringify({
-      hasBucket: !!env.BUCKET,
-      bucketType: env.BUCKET ? typeof env.BUCKET : null
-    }),
+      hasEnv: !!env,
+      envKeys: env ? Object.keys(env) : [],
+      hasBucket: !!env?.BUCKET,
+      bucketType: env?.BUCKET ? typeof env.BUCKET : null
+    }, null, 2),
     { headers: { "content-type": "application/json" } }
   );
 }
