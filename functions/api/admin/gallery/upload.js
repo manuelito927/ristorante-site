2dbc3b418f99b3a5dc926bc9efc47b422fde5adb
manuelito927 <filosofiaefficace@gmail.com>
2026-01-23 11:17:47 +0100

    Update upload.js

--- functions/api/admin/gallery/upload.js
+++ functions/api/admin/gallery/upload.js
@@ -1,6 +1,6 @@
 const CORS = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Methods": "POST, OPTIONS, GET",
 "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
