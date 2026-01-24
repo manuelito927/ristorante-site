69be533d204785d8e43146e156d4226feb15d7b1
manuelito927 <filosofiaefficace@gmail.com>
2026-01-24 11:50:12 +0100

    Update upload.js

--- functions/api/admin/gallery/upload.js
+++ functions/api/admin/gallery/upload.js
@@ -43,5 +43,4 @@ const url = `${origin}/api/public/image?key=${encodeURIComponent(key)}`;
   return new Response(JSON.stringify({ ok: true, key, url }), {
     headers: { "content-type": "application/json", ...CORS },
   });
-  }
 }
