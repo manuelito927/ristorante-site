f05961fd6f5ea6ee67a823f581540ab8069d8a52
manuelito927 <filosofiaefficace@gmail.com>
2026-01-26 18:29:32 +0100

    Delete functions/page/gallery.js

--- functions/page/gallery.js
+++
@@ -1,7 +0,0 @@
-export async function onRequestGet({ env }) {
-  const raw = await env.COVERS.get("GALLERY_LIST");
-  const urls = raw ? JSON.parse(raw) : [];
-  return new Response(JSON.stringify({ urls }), {
-    headers: { "content-type": "application/json" }
-  });
-}
