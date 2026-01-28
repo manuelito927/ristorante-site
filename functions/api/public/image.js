6ed71c7b90a471d8baeefa4302e7854e3111d3c9
manuelito927 <filosofiaefficace@gmail.com>
2026-01-28 12:53:48 +0100

    Update image.js

--- functions/api/public/image.js
+++ functions/api/public/image.js
@@ -11,7 +11,7 @@ export async function onRequestGet({ request, env }) {
   return new Response(obj.body, {
     headers: {
       "Content-Type": obj.httpMetadata?.contentType || "image/jpeg",
-      "Cache-Control": "public, max-age=31536000"
+"Cache-Control": "no-store"
     }
   });
 }
