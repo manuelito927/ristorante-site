54fb59dd3d70d897564655a803091956f37a573b
manuelito927 <filosofiaefficace@gmail.com>
2026-02-05 11:56:02 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1064,6 +1064,7 @@ renderReservations(allReservations);
       if (!reservations || reservations.length === 0) {
         reservationsGrid.innerHTML = "<div class='card'>Nessuna prenotazione trovata.</div>";
       }
+      // if (reservationsMsg) reservationsMsg.textContent = "OK";
     } catch (e) {
       reservationsGrid.innerHTML = `<div class="card">❌ Errore: ${escapeHtml(e.message)}</div>`;
       if (reservationsMsg) reservationsMsg.textContent = "Errore";
