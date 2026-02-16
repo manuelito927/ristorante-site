5d19fd776f9f559c196cd32e2b7dfeffc8e73aaf
manuelito927 <filosofiaefficace@gmail.com>
2026-02-17 00:25:00 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // js/config.js
 // Se non serve, lascialo così. Se serve, qui metti l'API del worker.
-window.API_BASE = "https://still-haze-01c8.filosofiaefficace.workers.dev";
+window.API_BASE = "";
